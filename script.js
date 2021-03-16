00e15dc358b03ece5bee6c9e61cff05b806d3ff2
Rachel Zoeller <rachel.zoeller@gmail.com>
2021-03-16 19:35:09 -0400

    updates to js, css

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
 
-  const subscriber_name = document.getElementById('subscriber_name')
-  const subscriber_email = document.getElementById('subscriber_email')
-  const form = document.getElementById('form')
-  const errorElement = document.getElementById('error')
+  // const name = document.getElementById('name')
+  // const email = document.getElementById('email')
+  // const form = document.getElementById('form')
+  // const errorElement = document.getElementById('error')
 
-  form.addEventListener('submit', (e) => {
-    let messages = []
-    if(subscriber_name.value === '' || subscriber_name.value == null)
-    messages.push('Name is Required')
+  // form.addEventListener('submit', (e) => {
+  //   let messages = []
+  //   if(name.value === '' || name.value == null)
+  //   messages.push('Name is Required')
 
-    if(subscriber_email.value === '' || subscriber_email.value == null)
-    messages.push('Please include @ in your email address')     
+  //   if(email.value === '' || email.value == null)
+  //   messages.push('Please include @ in your email address')     
 
-    if(messages.length) > 0 {
-    e.preventDefault()
-    errorElement.innerText = messages.join(', ')
-    }
+  //   if(messages.length) > 0 {
+  //   e.preventDefault()
+  //   errorElement.innerText = messages.join(', ')
+  //   }
+  // }) 
+
+ const form = document.querySelector('form');
+  const nameInput = document.querySelector(
+    'input[name="name"]'
+  );
+  
+  console.log(nameInput);
+  
+   form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("test");
+
   })
