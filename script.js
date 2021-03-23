613549a73505eae3a36734763bca2a037f485117
Rachel Zoeller <rachel.zoeller@gmail.com>
2021-03-22 20:37:54 -0400

    updates to css, js

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
 
-  // const name = document.getElementById('name')
-  // const email = document.getElementById('email')
-  // const form = document.getElementById('form')
-  // const errorElement = document.getElementById('error')
+   function emailIsValid () {
 
-  // form.addEventListener('submit', (e) => {
-  //   let messages = []
-  //   if(name.value === '' || name.value == null)
-  //   messages.push('Name is Required')
+    // Find Email input element
+      var emailInputElement = document.getElementById("email");
+    //Find name input element   
+    // Get value from email input element
+    //Get value from name input element 
+      var email = emailInputElement.value;
+    // Validate value against the rule
 
-  //   if(email.value === '' || email.value == null)
-  //   messages.push('Please include @ in your email address')     
+      var regex = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9])+.([a-z]+)(.[a-z]+)?$/; 
+      var validateResult = regex.test(email);
+    // If valid prompt successful message / Else prompt validation error message 
+      if (validateResult === true ) { 
+        //concantenate or add name value to message - join name to message
+        alert("Thanks");
+      }
+      else {
+        alert("Please enter a valid email address")
+      }
+   }
 
-  //   if(messages.length) > 0 {
-  //   e.preventDefault()
-  //   errorElement.innerText = messages.join(', ')
-  //   }
-  // }) 
-
- const form = document.querySelector('form');
-  const nameInput = document.querySelector(
-    'input[name="name"]'
-  );
+//  const form = document.querySelector('form');
+//   const nameInput = document.querySelector(
+//     'input[name="name"]'
+//   );
   
-  console.log(nameInput);
+//   console.log(nameInput);
   
-   form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    console.log("test");
+//    form.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     console.log('test');
 
-  })
+//   })
+
